125191e0a9e22ae6523eea4c6954bb7a8f91b457
sithija-ws <sithija.ws@gmail.com>
2026-07-12 12:49:34 +0530

    create roundbutton component

--- src/main/java/view/components/RoundedButton.java
+++ src/main/java/view/components/RoundedButton.java
@@ -1,3 +1,4 @@
+//rounded button component
 package view.components;
 
 import javax.swing.*;
@@ -14,7 +15,7 @@ public class RoundedButton extends JButton {
         this.radius = radius;
         this.normalBackgroundColor = bgColor;
         this.hoverBackgroundColor = bgColor.brighter();
-        
+
         setForeground(fgColor);
         setBackground(bgColor);
         setFocusPainted(false);
@@ -22,7 +23,7 @@ public class RoundedButton extends JButton {
         setOpaque(false);
         setBorderPainted(false);
         setFont(new Font("Segoe UI", Font.BOLD, 14));
-        
+
         addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 setBackground(hoverBackgroundColor);
@@ -34,10 +35,6 @@ public class RoundedButton extends JButton {
         });
     }
 
-    // Updates the button's "resting" color (used when the mouse is not hovering).
-    // Needed so external code (e.g. RoleTogglePanel) can change which state
-    // a toggle button rests in, instead of it always snapping back to the
-    // color it was originally constructed with.
     public void setRestingBackground(Color bgColor) {
         this.normalBackgroundColor = bgColor;
         this.hoverBackgroundColor = bgColor.brighter();
@@ -48,10 +45,10 @@ public class RoundedButton extends JButton {
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        
+
         g2.setColor(getBackground());
         g2.fill(new RoundRectangle2D.Float(0, 0, getWidth(), getHeight(), radius, radius));
-        
+
         super.paintComponent(g);
         g2.dispose();
     }
